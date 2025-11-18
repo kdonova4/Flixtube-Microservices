00af2c1aa63e4b4f282172111d17d6caf1ecfba2
Kevin Donovan <donovankevin58@gmail.com>
2025-11-18 02:03:39 -0500

    Committing chapter 6

--- flixtube/video-streaming/src/index.js
+++ flixtube/video-streaming/src/index.js
@@ -1,69 +1,38 @@
 const express = require("express");
 const fs = require("fs");
-const http = require("http");
-const amqp = require('amqplib');
 
+const app = express();
+
+//
+// Throws an error if the PORT environment variable is missing.
+//
 if (!process.env.PORT) {
     throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
 }
 
-if (!process.env.RABBIT) {
-    throw new Error("Please specify the name of the RabbitMQ host using environment variable RABBIT");
-}
-
+//
+// Extracts the PORT environment variable.
+//
 const PORT = process.env.PORT;
-const RABBIT = process.env.RABBIT;
-
 
 //
-// Application entry point.
+// Registers a HTTP GET route for video streaming.
 //
-async function main() {
+app.get("/video", async (req, res) => { // Route for streaming video.
     
-    console.log(`Connecting to RabbitMQ server at ${RABBIT}`);
+    const videoPath = "./videos/loveland_td.mp4";
+    const stats = await fs.promises.stat(videoPath);
 
-    const messagingConnection = await amqp.connect(RABBIT); // connects to the RabbitMQ server
-
-    console.log("Connected to RabbitMQ");
-
-    const messageChannel = await messagingConnection.createChannel(); // Creates a RabbitMq messaging channel
-
-    await messageChannel.assertExchange("viewed", "fanout"); // asserts that we have a "viewed" exchange
-
-    // Send the "viewed" to the history microservice
-    function broadcastViewedMessage(messageChannel, videoPath) {
-        console.log("Publishing message on 'viewed' queue");
-
-        const msg = { videoPath: videoPath };
-        const jsonMsg = JSON.stringify(msg);
-        messageChannel.publish("viewed", "", Buffer.from(jsonMsg)) // publishes message to the 'viewed' exchange
-    }
-
-    const app = express();
-
-    app.get("/video", async (req, res) => {
-        const videoPath = "./videos/loveland_td.mp4";
-        const stats = await fs.promises.stat(videoPath);
-
-        res.writeHead(200, {
-            "Content-Length": stats.size,
-            "Content-Type": "video/mp4",
-        });
-
-        fs.createReadStream(videoPath).pipe(res);
-
-        broadcastViewedMessage(messageChannel, videoPath)
+    res.writeHead(200, {
+        "Content-Length": stats.size,
+        "Content-Type": "video/mp4",
     });
+    fs.createReadStream(videoPath).pipe(res);
+});
 
-
-
-    app.listen(PORT, () => {
-        console.log("Microservice online.");
-    });
-}
-
-main()
-    .catch(err => {
-        console.error("Microservice failed to start.");
-        console.error(err && err.stack || err);
-    });
+//
+// Starts the HTTP server.
+//
+app.listen(PORT, () => {
+    console.log(`Microservice online.`);
+});
